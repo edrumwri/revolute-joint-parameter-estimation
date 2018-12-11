0ee108e9236753501eba543781f76b5d6cd5d5e2
Roxana Leontie <roxana@gwu.edu>
2018-12-11 15:34:33 -0500

    based on Matlab programming style guide function header should include the input variables

--- getHollowSphereInertiaTensor.m
+++ getHollowSphereInertiaTensor.m
@@ -1,6 +1,6 @@
 function J_B = getHollowSphereInertiaTensor(mass, sphere_radius)
-% getHollowSphereInertiaTensor function that defines the inertia tensor for
-% a 3D hollow sphere of mass "mass" and radius "sphere_radius".
+% getHollowSphereInertiaTensor(mass, sphere_radius) function that defines 
+% the inertia tensor for a 3D hollow sphere of mass "mass" and radius "sphere_radius".
 
 J_B = eye(3) * 2/3 * mass * sphere_radius^2;
 end

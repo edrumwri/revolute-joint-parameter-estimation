6dfcfe427a6528e8e35551e6534a8d58ddd7db65
Roxana Leontie <roxana@gwu.edu>
2018-12-11 14:31:50 -0500

    updated based on review

--- TestHollowSphereInertiaTensor.m
+++ TestHollowSphereInertiaTensor.m
@@ -1,6 +1,6 @@
 classdef TestHollowSphereInertiaTensor < matlab.unittest.TestCase
-    %TestHollowSphereInertiaTensor Unit testing for getHollowSphereInertiaTensor
-    %   this runs a sanity test, the hollow Sphere Inertia matrix has a
+    % TestHollowSphereInertiaTensor Unit testing for getHollowSphereInertiaTensor
+    %   this runs a sanity test, the hollow sphere inertia matrix has a
     %   closed form solution
     
     properties
@@ -11,9 +11,9 @@ classdef TestHollowSphereInertiaTensor < matlab.unittest.TestCase
             %since this is a closed form solution
             mass = 10;
             sphere_radius = 1;
-            actSol = getHollowSphereInertiaTensor(mass,sphere_radius);
+            actSol = getHollowSphereInertiaTensor(mass, sphere_radius);
             expSol = eye(3) * 2/3 * mass * sphere_radius^2;
-            testCase.verifyEqual(actSol,expSol,'AbsTol',sqrt(eps));
+            testCase.verifyEqual(actSol, expSol, 'AbsTol', sqrt(eps));
         end 
     end
 end
--- getHollowSphereInertiaTensor.m
+++ getHollowSphereInertiaTensor.m
@@ -1,7 +1,6 @@
-function J_B = getHollowSphereInertiaTensor(mass,sphere_radius)
-%getHollowSphereInertiaTensor function that defines the inertia tensor for
-%a 3D hollow sphere of mass "mass" and radius sphere_radius
+function J_B = getHollowSphereInertiaTensor(mass, sphere_radius)
+% getHollowSphereInertiaTensor function that defines the inertia tensor for
+% a 3D hollow sphere of mass "mass" and radius "sphere_radius"
 
-J_B = diag([2/3 * mass * sphere_radius^2, 2/3 * mass * sphere_radius^2, 2/3 * mass * sphere_radius^2]);
-
+J_B = eye(3)* 2/3 * mass * sphere_radius^2;
 end

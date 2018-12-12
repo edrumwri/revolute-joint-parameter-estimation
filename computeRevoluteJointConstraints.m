21752da7aba8ed5cdfd32baa057c3610e2d5146b
Roxana Leontie <roxana@gwu.edu>
2018-12-12 12:46:40 -0500

    updated some variable names and description

--- computeRevoluteJointConstraints.m
+++ computeRevoluteJointConstraints.m
@@ -1,13 +1,15 @@
-function revoluteConstraints = computeRevoluteJointConstraints(ui, vi, vj, y)
+function revoluteConstraints = computeRevoluteJointConstraints(ui, vi, vj,  pose)
 % computeRevoluteJointConstraints(ui, uj, quat) computes the revolute joint 
 % constraints as spherical joint constraints with two aditional constraints.
-%   ui is a 3x1 constant vector in the frame of link i.
+%   ui is a 3x1 local position vector (on body i) defining the joint
+%   location wrt the grasping point.
 %   vi is a 3x1 vector for the revolute joint in the body i frame.
 %   vj is a 3x1 vector for the revolute joint in the body j frame (fixed to the world).
-%   y is the position and orientation 7x1 vector.
+%   y is a7x1 vector representing the position and orientation of the 
+%   grasping point in the global frame.
 
-    x = y(1:3); % position components
-    quat = y(4:7); % quaternion components
+    x = pose(1:3); % position components
+    quat = pose(4:7); % quaternion components
     wRi = quat2R(quat);
     wRj = eye(3); % the 2 body (j) is attached to the world
     
--- computeSphericalJointConstraints.m
+++ computeSphericalJointConstraints.m
@@ -1,9 +1,13 @@
-function sphericalConstraints = computeSphericalJointConstraints(ui, y)
-% computeSphericalJointConstraints(u, y) computes the spherical joint
-% constraints.
-%   ui is a constant vector in the frame of link i.
-    x = y(1:3); % position components
-    quat = y(4:7); % quaternion components
+function sphericalConstraints = computeSphericalJointConstraints(ui, pose)
+% computeSphericalJointConstraints(ui, y) computes the spherical joint
+% constraints f = x + R * u.
+%   ui is a 3x1 local position vector (on body i) defining the joint
+%   location wrt the grasping point.
+%   pose is a7x1 vector representing the position and orientation of the 
+%   grasping point in the global frame.
+
+    x = pose(1:3); % position components
+    quat = pose(4:7); % quaternion components
     wRi = quat2R(quat);
     sphericalConstraints = x + wRi * ui;
     

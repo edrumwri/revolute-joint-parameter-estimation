770c3a8268204e737e942f9ed4f309bcdebe0333
Roxana Leontie <roxana@gwu.edu>
2018-12-11 21:13:42 -0500

    updated the headers

--- computeSphericalJointConstraints.m
+++ computeSphericalJointConstraints.m
@@ -5,7 +5,7 @@ function RevConstr = computeSphericalJointConstraints(u, y)
     x = y(1:3); %position components
     quat = y(4:7); %quaternion components
     wRi = quat2R(quat);
-    RevConstr = x + wRi*u;
+    RevConstr = x + wRi * u;
     
 end
 

d8632d5f9b6652fe7b487383585c1c3b5051cd0e
Roxana Leontie <roxana@gwu.edu>
2018-12-12 12:29:47 -0500

    updated constraint functions; added the spherical constraints to the revolute

--- computeRevoluteJointConstraints.m
+++ computeRevoluteJointConstraints.m
@@ -1,11 +1,26 @@
-function cross = computeRevoluteJointConstraints(ui, uj, quat)
-% computeRevoluteJointConstraints(ui, uj, quat) returns the cross product of
-% vectors ui and uj.
-%   vector ui is in the body i frame .
-%   vector uj is in the global frame (fixed to the world).
+function revoluteConstraints = computeRevoluteJointConstraints(ui, vi, vj, y)
+% computeRevoluteJointConstraints(ui, uj, quat) computes the revolute joint 
+% constraints as spherical joint constraints with two aditional constraints.
+%   ui is a 3x1 constant vector in the frame of link i.
+%   vi is a 3x1 vector for the revolute joint in the body i frame.
+%   vj is a 3x1 vector for the revolute joint in the body j frame (fixed to the world).
+%   y is the position and orientation 7x1 vector.
 
-   wRi = quat2R(quat);
-   wRj = eye(3); % the 2 body (j) is attached to the world
-   cross = (wRi*ui)' * (wRj*uj);
+    x = y(1:3); % position components
+    quat = y(4:7); % quaternion components
+    wRi = quat2R(quat);
+    wRj = eye(3); % the 2 body (j) is attached to the world
+    
+    % Compute global vectors.
+    viw = wRi * vi; 
+    vjw = wRj * vj; 
+    
+    % Formulate the parallelism condition of the two vectors wvi and wvj as
+    % two independent dot product equations.
+    [v1iw, v2iw] = compute2OrtogonalVect(viw);
+    revolute_parallel_vect = [v1iw' * vjw; v2iw' * vjw];
+    
+    sphericalConstraints = x + wRi * ui;
+    
+    revoluteConstraints = [sphericalConstraints; revolute_parallel_vect];
 end
-
--- computeSphericalJointConstraints.m
+++ computeSphericalJointConstraints.m
@@ -1,11 +1,11 @@
-function RevConstr = computeSphericalJointConstraints(u, y)
+function sphericalConstraints = computeSphericalJointConstraints(ui, y)
 % computeSphericalJointConstraints(u, y) computes the spherical joint
-% constraints
-
-    x = y(1:3); %position components
-    quat = y(4:7); %quaternion components
+% constraints.
+%   ui is a constant vector in the frame of link i.
+    x = y(1:3); % position components
+    quat = y(4:7); % quaternion components
     wRi = quat2R(quat);
-    RevConstr = x + wRi * u;
+    sphericalConstraints = x + wRi * ui;
     
 end
 

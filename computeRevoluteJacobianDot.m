b8388aea02e95719dd1513b8d0a0f3a4b88c08e7
Roxana Leontie <roxana@gwu.edu>
2018-12-14 16:03:37 -0500

    JacobianDot files for revolute and spherical

---
+++ computeRevoluteJacobianDot.m
@@ -0,0 +1,32 @@
+function JRevDot = computeRevoluteJacobianDot(ui, vi, vj, quat, quatDot)
+% computeRevoluteJacobianDot analytical computation of the dot{jacobian}
+% for the revolute joint.
+%   The jacobian is = [  eye(3)      -skew(wRi * ui); 
+%                      zeros(1,3)   v1i' * wRi' * skew(vj);
+%                      zeros(1,3)   v2i' * wRi' * skew(vj);
+%
+%   where wRi = qt2rot(quat) and vectors vi1 and vi2 form the orthogonal 
+%   triad vi, v1i and v2i
+
+  JSphericalDot = computeSphericalJacobianDot(ui, quat, quatDot);
+  
+  [v1i, v2i] = computeBasisFromAxis(vi);   
+    
+  JRevConstDot1 = computeRevConstrDot(v1i, vj, quat, quatDot);
+  JRevConstDot2 = computeRevConstrDot(v2i, vj, quat, quatDot);
+    
+  JRevDot = [JSphericalDot; JRevConstDot1; JRevConstDot2];
+
+end
+
+function JRevConstrDot = computeRevConstrDot(vxi, vj, quat, quatDot)
+% Uses the analytical formulation to compute the dot constraint.
+
+  qw = quat(1); qx = quat(2); qy = quat(3); qz = quat(4); 
+  qwDot = quatDot(1); qxDot = quatDot(2); qyDot = quatDot(3); qzDot = quatDot(4);
+  JRevConstrDot = [0, 0, 0, ...
+ -2*vj(3)*((qxDot*qy + qx*qyDot + qwDot*qz + qw*qzDot)*vxi(1) + 2*(qw*qwDot + qy*qyDot)*vxi(2) + (-(qwDot*qx) - qw*qxDot + qyDot*qz + qy*qzDot)*vxi(3)) + 2*vj(2)*((-(qwDot*qy) - qw*qyDot + qxDot*qz + qx*qzDot)*vxi(1) + (qwDot*qx + qw*qxDot + qyDot*qz + qy*qzDot)*vxi(2) + 2*(qw*qwDot + qz*qzDot)*vxi(3)), ...
+  2*vj(3)*(2*(qw*qwDot + qx*qxDot)*vxi(1) + (qxDot*qy + qx*qyDot - qwDot*qz - qw*qzDot)*vxi(2) + (qwDot*qy + qw*qyDot + qxDot*qz + qx*qzDot)*vxi(3)) - 2*vj(1)*((-(qwDot*qy) - qw*qyDot + qxDot*qz + qx*qzDot)*vxi(1) + (qwDot*qx + qw*qxDot + qyDot*qz + qy*qzDot)*vxi(2) + 2*(qw*qwDot + qz*qzDot)*vxi(3)), ...
+ -2*vj(2)*(2*(qw*qwDot + qx*qxDot)*vxi(1) + (qxDot*qy + qx*qyDot - qwDot*qz - qw*qzDot)*vxi(2) + (qwDot*qy + qw*qyDot + qxDot*qz + qx*qzDot)*vxi(3)) + 2*vj(1)*((qxDot*qy + qx*qyDot + qwDot*qz + qw*qzDot)*vxi(1) + 2*(qw*qwDot + qy*qyDot)*vxi(2) + (-(qwDot*qx) - qw*qxDot + qyDot*qz + qy*qzDot)*vxi(3))];
+
+end

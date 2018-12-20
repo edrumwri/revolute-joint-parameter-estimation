ced83748dc237fd80d4044b5d0ce4399fbce3ffa
Roxana Leontie <roxana@gwu.edu>
2018-12-20 11:35:50 -0500

    fixed spelling

--- computeRevoluteJacobianDotV.m
+++ computeRevoluteJacobianDotV.m
@@ -2,7 +2,7 @@ function JacDotTimesVel = computeRevoluteJacobianDotV(ui, vi, vj, pose, velocity
 % computeRevoluteJacobianDotV computes numerically \dot{Jacobian} * velocity
 %   Based on the formula:
 %   \dot{f}     = J * v, 
-%   \ddot{f}    = \dot{J} * v + J * \dot{v}, we than have
+%   \ddot{f}    = \dot{J} * v + J * \dot{v}, we then have
 %   \dot{J} * v =  \ddot{f} - J * \dot{v}.
 
     [fRevolute, fDRevolute, fDDRevolute] = computeRevoluteJointConstraints(ui, vi, vj, pose, velocity, acceleration);

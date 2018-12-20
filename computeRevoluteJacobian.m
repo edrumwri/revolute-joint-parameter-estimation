d9fa3b88ee35653931a34f91a8fb93e4889284b7
Roxana Leontie <roxana@gwu.edu>
2018-12-19 19:14:43 -0500

    fixed transpose error

--- computeRevoluteJacobian.m
+++ computeRevoluteJacobian.m
@@ -9,14 +9,14 @@ function JRev = computeRevoluteJacobian(ui, vi, vj, quat)
 %   The revolute constraint derivation for vxi stands for either v1i or v2i.
 
 %   JRev = [eye(3)     -skew(wRi * ui); ...        % Spherical Jacobian
-%           zeros(1,3) vjw' * -skew(wRi * v1i)'; ...
-%           zeros(1,3) vjw' * -skew(wRi * v2i)']
+%           zeros(1,3) vjw' * -skew(wRi * v1i); ...
+%           zeros(1,3) vjw' * -skew(wRi * v2i)]
 %
 %   where wRi = qt2rot(quat) and vectors vi1 and vi2 form the orthogonal 
 %   triad vi, v1i and v2i
 %
 %   Since there are no linear velocity components in the \dot{f} the first 3 terms are 0
-%   and the rest are the angular velocity terms vjw' * -skew(wRi * ui)
+%   and the rest are the angular velocity terms vjw' * -skew(wRi * vxi)
 %   for the revolute constraints in the resulting jacobian. 
 
 JSpherical = computeSphericalJacobian(ui, quat);
@@ -27,7 +27,7 @@ JSpherical = computeSphericalJacobian(ui, quat);
  skew_v2iw = getSkewSymmetricMatrix(wRi * v2i);
  
  JRev = [JSpherical; ...
-         zeros(1,3) vj' * -skew_v1iw';...
-         zeros(1,3) vj' * -skew_v2iw'];
+         zeros(1,3) vj' * -skew_v1iw;...
+         zeros(1,3) vj' * -skew_v2iw];
 end
 
